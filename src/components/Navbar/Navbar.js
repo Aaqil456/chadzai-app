9e0d0893f8df59b4bdf741b1b11037ef542c8906
Aaqil Ahamad <72489957+Aaqil456@users.noreply.github.com>
2024-11-03 17:35:19 +0800

    changes hmaburger menu design in mobile

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -30,19 +30,11 @@ const Navbar = () => {
         <img src={logo} alt="Logo" />
       </div>
       <div className="hamburger" onClick={toggleMenu}>
-        {/* <span></span>
-        <span></span> */}
+        <span></span>
+        <span></span>
         <span></span>
       </div>
       <div className={`navbar-links ${isMenuOpen ? 'active' : ''}`}>
-        <ul>
-          {/* <li>
-            <Link to="/main-menu" onClick={() => setIsMenuOpen(false)}>Home</Link>
-          </li>
-          <li>
-            <Link to="/chatbot" onClick={() => setIsMenuOpen(false)}>Image Recognition</Link>
-          </li> */}
-        </ul>
         <button onClick={handleLogout}>Logout</button>
       </div>
     </nav>
